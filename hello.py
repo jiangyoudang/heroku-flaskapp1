0bee461854c432fe754b9db081f17b5685d32836
cong <893101471@qq.com>
2015-01-27 01:42:59 -0800

    add db

--- hello.py
+++ hello.py
@@ -1,9 +1,27 @@
 from flask import Flask
 app = Flask(__name__)
 
+from flask import render_template
+
+########################
+#route dispatch
+
+import json
+from database import db
+
+
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return render_template('base.html', title='Home')
+
+
+
+@app.route('/userlist')
+def get_user_list():
+    d = db.get_user()
+    result = json.dumps(d)
+    return result
+
 
 if __name__ == "__main__":
     app.run()
